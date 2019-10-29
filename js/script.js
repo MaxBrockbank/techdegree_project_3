4f3fd9be341d044069460712009e4647b6d99eda
Max Brockbank <maxbrockbank1999@gmail.com>
2019-10-28 19:10:33 -0700

    prevent default

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ const $email = $("#mail");
 const $ccNum = $("#cc-num");
 const $ccZip = $("#zip");
 const $ccCVV = $("#cvv");
+//Name Regex from https://andrewwoods.net/blog/2018/name-validation-regex/
+const nameValid = /^[A-Za-z]+([\ A-Za-z]+)*/;
+// Email Regex from https://www.emailregex.com
+const emailValid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// Credit Card Regex from https://www.regular-expressions.info/creditcard.html
+const ccValid = /^\b\d{13,16}\b$/;
+
+const zipValid = /^\d{5}$/;
+
+const cvvValid = /^\d{3,4}$/;
 //Set focus on Name input
 $name.focus();
 
@@ -62,12 +72,12 @@ $(".activities").change(function(e){
     }
   })
 
-  //error message to make a selection
+  //error message for activity section
 
   if($(".activities input:checkbox:checked").length >= 1){
-  $("#nah").remove();
+  $("#activityError").remove();
   } else if($(".activities input:checkbox:checked").length < 1) {
-    $(".activities legend").append("<label id='nah' class = 'error'>Activity Selection Required<label>")
+    $(".activities legend").append("<label id='activityError' class = 'error'>Activity Selection Required<label>")
   }
 
 });
@@ -95,45 +105,69 @@ $("#payment").change(function(e){
 
 //Form Validation
 
-//validators//
-  //Name Regex from https://andrewwoods.net/blog/2018/name-validation-regex/
-  const nameValid = /^[A-Za-z]+([\ A-Za-z]+)*/;
-  // Email Regex from https://www.emailregex.com
-  const emailValid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  // Credit Card Regex from https://www.regular-expressions.info/creditcard.html
-  const ccValid = /^\b\d{13,16}\b$/;
-
-  const zipValid = /^\d{5}$/;
-
-  const cvvValid = /^\d{3,4}$/;
-
 //form validation function
-  function inputValidation (element, regex, message, id){
-    let $id ="#"+ id;
-    if(regex.test($(element).val())){
-      $($id).remove();
-    } else if(regex.test($(element).val()) === false && element.prev().attr('id')!== id) {
-      $(element).before("<label id = "+id+" class = 'error'></label>");
-      $(element).prev().text(message);
+function inputValidation (element, regex, message, id){
+  let $id ="#"+ id;
+  if(regex.test($(element).val())){
+    $($id).remove();
+  } else if(regex.test($(element).val()) === false && element.prev().attr('id')!== id) {
+    $(element).before("<label id = "+id+" class = 'error'></label>");
+    $(element).prev().text(message);
+  }
+}
+
+// validators passed into the form validation function
+$name.on('input', function(){
+  inputValidation($name, nameValid, "Name Required", "nameError")
+});
+$email.on('input', function(){
+  inputValidation($email, emailValid, "Valid E-mail Address Required", "emailError")
+});
+$ccNum.on('input', function(){
+  if($ccNum.val().length <1){
+  $("#ccnError1").remove();
+  inputValidation($ccNum, ccValid, "Please Enter Credit Card Number", "ccnError0");
+}else{
+  $("#ccnError0").remove();
+  inputValidation($ccNum, ccValid, "Must Be 13-16 Characters", "ccnError1");
+  }
+});
+$ccZip.on('input', function(){
+  if($ccZip.val().length < 1){
+  $("#zipError1").remove();
+  inputValidation($ccZip, zipValid, "Zip Code Required", "zipError0")
+  }else {
+  $("#zipError0").remove();
+  inputValidation($ccZip, zipValid, "Must Be 5 Digits", "zipError1")
+  }
+});
+$ccCVV.on('input', function(){
+  if($ccCVV.val().length < 1){
+  $("#cvvError1").remove();
+  inputValidation($ccCVV, cvvValid, "CVV Required", "cvvError0")
+  }else{
+  $("#cvvError0").remove();
+  inputValidation($ccCVV, cvvValid, "Must Be 3-4 Digits", "cvvError1")
+  }
+});
+
+// input check function
+
+//prevent default on submit button
+function validInput(inputElement, regex) {
+  if(inputElement.val()==""|| regex.test(inputElement.val()) === false){
+    return false;
+  } else{
+    return true;
+  }
+};
+
+$("form").on('submit',function(event){
+  if(validInput($name, nameValid) === false || validInput($email, emailValid) === false|| $(".activities input:checkbox:checked").length < 1 || ($("#payment").val() === "Credit Card" && (validInput($ccNum, ccValid) === false||validInput($ccZip, zipValid) === false||validInput($ccCVV, cvvValid) === false))){
+    event.preventDefault();
+    $("input:empty").addClass('border-error')
+    if($(".activities:checkbox:checked").length < 1){
+      $(".activities legend").addClass("error");
     }
   }
-// validators passed into the form validation function
-  $name.on('input', function(){
-    inputValidation($name, nameValid, "Name Required", "nameError")
-  });
-  $email.on('input', function(){
-    inputValidation($email, emailValid, "Valid E-mail Address Required", "emailError")
-  });
-  $ccNum.on('input', function(){
-    inputValidation($ccNum, ccValid, "Credit Card Number Must Be 13-16 Characters", "ccnError")
-  });
-  $ccZip.on('input', function(){
-    inputValidation($ccZip, zipValid, "Zip Code Required", "zipError")
-  })
-  $ccCVV.on('input', function(){
-    inputValidation($ccCVV, cvvValid, "CVV Required", "cvvError")
-  })
-//Validation Messages/
-
-
-//Extra Credit
+});
